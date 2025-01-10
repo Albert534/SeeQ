64c8a35c64f0ff6e5a6104544596f05e09d7809d
Aung Paing Oo <oaungpaing56@gmail.com>
2025-01-10 19:47:46 +0630

    Signup UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				notos: ['Noto Sans'],
+				'notos-mm': ['Noto Sans Myanmar'],
+			},
+
+			boxShadow: {
+				'custom-zero': '0 0 10px rgba(0, 0, 0, 0.5)',
+			},
+			colors: {
+				'primary-main': '#3D3BF3',
+				'primary-pale': '#6B69F6',
+				'primary-thick': '#2826CB',
+				'input-bg': '#3F3F46',
+			},
+		},
 	},
 	plugins: [],
 };
